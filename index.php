3bb6c90a9d15e1c2d6de33d87ba39b10581f74d6
ryanhoang10 <55223126+ryanhoang10@users.noreply.github.com>
2020-03-08 19:05:36 -0500

    First commit

---
+++ index.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+      
+    <link href="https://fonts.googleapis.com/css?family=Arvo&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" href="styles.css">
+
+    <title>Online Notes</title>
+  </head>
+  <body>
+    <!-- Navigation Bar -->
+      <nav role="navigation" class="navbar navbar-custom navbar-fixed-top">
+          
+          <div class="container-fluid">
+          
+              <div class="navbar-header">
+              
+                <a class="navbar-brand">Online Notes</a>
+                  
+                  
+                <button type="button" class="navbar-toggle" data-target="#navbarCollapse" data-toggle="collapse">    
+                  
+                    <span class="sr-only">Toggle navigation</span>
+                  
+                    <span class="icon-bar"></span>
+                  
+                    <span class="icon-bar"></span>
+                  
+                    <span class="icon-bar"></span>
+                </button>
+                  
+              </div>
+              <div class="navbar-collapse collapse" id="navbarCollapse">
+              
+                  <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="#">Help</a></li>
+                    <li><a href="#">Contact</a></li>
+                  </ul>
+                  
+                   <ul class="nav navbar-nav navbar-right"> 
+                    <li><a href="#">Login</a></li>               
+                  </ul>
+                  
+              </div>
+          </div>
+      
+      </nav>
+    <!-- Jumbotron with Sign up Button -->
+      
+      <div class="jumbotron" id="myContainer">
+          <h1>Online Notes</h1>
+          <p>Your notes with you wherever</p>
+          <p>Easy to use!</p>
+          
+          <button type="button" class="btn btn-lg green signup">Sign Up</button>
+      </div>
+      
+    <!-- Login form -->
+      
+    <!-- Sign up form -->
+      
+    <!-- Maybe forget Password -->
+    
+    <!-- Footer -->
+      
+      <div class="footer">
+        <div class="container">
+          <p>Ryan Hoang CSIS484 &copy; 2019-<?php $today=date("Y"); echo $today?>.</p>
+        </div>
+      </div>
+      
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
